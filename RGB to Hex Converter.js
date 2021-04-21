3e599f335f93cd1a3f914bff053e920b052ae6ae
Bowei Qiu <z32498926@gmail.com>
2021-04-21 12:34:30 +0800

    add 進制轉換功能

---
+++ RGB to Hex Converter.js
@@ -0,0 +1,32 @@
+const HEX = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+const hexNumber = []
+function RGBelement() {
+  let R = document.querySelector('.inR').value
+  console.log(R)
+  let G = document.querySelector('.inG').value
+  console.log(G)
+  let B = document.querySelector('.inB').value
+  console.log(B)
+}
+
+function convert(decimal) {
+  let a = parseInt(decimal / 16)  //先除一遍用parseInt取整數，捨去小數
+
+  //decimal 2 hex 參考資料 >>> https://www.footmark.info/introduction-to-computer/digital-system-conversion/#fm-chapter-3-3-1
+  let b = decimal % 16
+  while (a !== 0) {
+    let HEXword = HEX[b]
+    hexNumber.unshift(HEXword)  //推進陣列
+    b = a % 16
+    a = parseInt(a / 16)
+    if (a === 0) {              //若a為零還要再輸出一次餘數，否則會少輸出一個
+      HEXword = HEX[b]
+      hexNumber.unshift(HEXword)
+    }
+  }
+  return hexNumber
+}
+
+console.log(convert(255))
+//後續會用到清掉陣列內容以便下一個數字轉換後可填入
+hexNumber.splice(0, hexNumber.length)
